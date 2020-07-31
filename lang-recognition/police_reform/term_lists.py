f3c5e28c2b7bcdb1f82e1f20144f737c25f6eb63
kgrant20 <68939618+kgrant20@users.noreply.github.com>
2020-07-30 20:17:42 -0400

    Create term_lists.py

---
+++ lang-recognition/police_reform/term_lists.py
@@ -0,0 +1,73 @@
+#this file contains each category for police reform and its term lists 
+#the categories with two term lists are a result of a lack of text and a need for higher weighted words
+
+#Use of Force Data Collection
+forceTerms = ['collect','data','report','description']
+
+#data avilability
+availabilityTerms = ['public','review','available','access','release']
+
+#data retention 
+retentionTerms = ['require','year','data','footage','report']
+
+#body-worn cameras required
+cameraTerms = ['require','car','video','audio','activate']
+
+#anti-fraud provisions 
+fraudTerms = ['fals','intent','obstruction','conceal','cover up','offense']
+
+#whistleblowers 
+whistTerms = ['authorize','right','disclose']
+
+#requirement to intervene 
+interveneTerms1 = ['excessive']
+interveneTerms = ['force','training','clear','duty','file','fulfill']
+
+#Accountability
+#Enforcement of compliance
+enforcementTerms = ['grant','require','reallocat','fund','ensure','fail','reduction','suspend']
+
+#handling/investigating reports of abuse 
+reportsTerms = ['retain','review','investigat','maintain','record','report','force','retention']
+
+#qualified immunity restriction 
+immunityTerms1 = ['reform','amend']
+immunityTerms2 = ['add','not be a defense']
+
+#restricting chokeholds 
+chokeholdTerms1 = ['ban','prohibit']
+chokeholdTerms2 = ['incentiviz','prevent','civil rights violation','danger']
+
+#restricting use of force
+lessforceTerms1 = ['avoid', 'restrict']
+lessforceTerms2 = ['alternatives','dangerous','reduction','de-escalat','train']
+
+#no-knock warrant banning 
+warrantTerms1 = ['ban','prohibit']
+warrantTerms2 = ['report','danger','require']
+
+#lynching federal crime 
+lynchTerms = ['federal crime','crime']
+
+#hiring background requirements *dem should be higher
+hireTerms1 = []
+hireTerms2 = ['require','study','implement','psychological','standards','disciplinary record','grant']
+
+#hiring diversity requirements
+divTerms = ['diverse','diversity','communit','represent']
+
+#TRAINING
+#use of force training
+forcetrainTerms = ['training','require','regulat','procedure','standards','program']
+
+#de-escalation training
+deecsTerms1 = ['training','effort']
+deescTerms = ['technique','minimize','program','avoid','develop']
+
+#anti-descrimmination training 
+antidescTerms1 = ['training']
+antidescTerms = ['racial','awareness','program','standards']
+
+#requirements for victim services 
+victimTerms1 = ['provide']
+victimTerms = ['service','counseling','protect']
