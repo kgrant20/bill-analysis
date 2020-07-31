cf0ae3696f3a13bbd2a645cb223e611a11cc08bb
kgrant20 <68939618+kgrant20@users.noreply.github.com>
2020-07-30 20:26:13 -0400

    Create term_lists.py

---
+++ lang-recognition/gun_violence/term_lists.py
@@ -0,0 +1,51 @@
+#this file contains just the gun violence bills term lsits 
+#some cateogries have 2 term lists, indicating a lack of text and a need for higher weighted terms
+
+#data avilability
+availabilityTerms = ['review','available','require','allow','access']
+
+#Federal/violation record Collection
+forceTerms = ['submit','available','consistent','collect','report','description','submit','provide']
+
+#data retention 
+retentionTerms = ['annual','require','year','consistent','database','maintain']
+
+#anti-fraud provisions 
+fraudTerms1 = ['false','penalty']
+fraudTerms = ['prohibit','lying','risk protection','denial']
+
+#Accountability
+#increasing Federal prosecution of gun violence
+enforcementTerms = ['prosecute','investigat','penalt','ban','imprisonment','fine','revoke','seizure']
+
+#straw purchasing
+enforcementTerms1 = ['prohibit','illegal']
+enforcementTerms = ['unlawful','investigat','penalt','ban','offense','violat']
+
+#trafficking **dem uses various wording to describe
+enforcementTerms1 = ['prohibit','illegal']
+enforcementTerms = ['unlawful','investigat','penalt','ban','offense','violat']
+
+#assault weapons ban 
+enforcementTerms1 = ['ban','prohibit','illegal']
+enforcementTerms = ['unlawful','investigat','penalt','restrict','offense','violat']
+
+#silencer/muffler ban
+enforcementTerms1 = ['prohibit','illegal','ban']
+enforcementTerms = ['silencer','muffler','penalt']
+
+#DEALER REFORM
+#shop security
+reportsTerms = ['safe','secure','investigat','maintain','record','report','force','licensed dealer']
+
+#DEALER REFORM
+#employee check
+reportsTerms = ['safe','secure','background check','qualified','test','report','enforce','licensed dealer']
+
+#INDUSTRY REFORM
+# firearm transport / employee protection
+reportsTerms = ['safe','secure','protect','qualified','test','authorize','enforce','lawful','violat']
+
+#gun violence study
+lessforceTerms1 = ['study', 'research']
+lessforceTerms2 = ['alternatives','intervention','reduction','program']
